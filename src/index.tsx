dc3445fc30f24c970745b9da306a27e56f6f5305
bally <108769285+ballyalley-o@users.noreply.github.com>
2024-02-09 09:04:47 +1300

    Refactor Redux setup and remove unused code

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+// @redux
 import { createStore, applyMiddleware, Reducer } from 'redux'
 import { Provider } from 'react-redux'
+import { reducers } from './store/reducers'
 import thunk from 'redux-thunk'
+// @components
 import { App } from './components'
 import { IApiFetchAction } from './interfaces/api-fetch'
-// Update the type of the state in the reducers object
-import { reducers } from './store/reducers'
-
-// const store = createStore<
-//   RootState,
-//   IApiFetchAction,
-//   Partial<{ fetchApi: never }>
-// >(reducers, applyMiddleware(thunk))
 
 const store = createStore(reducers, applyMiddleware(thunk))
 
 const el = document.getElementById('root') as HTMLElement
 const root = ReactDOM.createRoot(el!)
+
 root.render(
   <React.StrictMode>
     <Provider store={store}>
@@ -25,8 +21,3 @@ root.render(
     </Provider>
   </React.StrictMode>
 )
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals()
