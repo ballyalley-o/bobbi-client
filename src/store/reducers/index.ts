e1d1e2a45559e969e27ca2d95a9a616f0891c526
bally <108769285+ballyalley-o@users.noreply.github.com>
2024-02-09 09:07:26 +1300

    Update fetchApi reducer name to fetchApiData

--- src/store/reducers/index.ts
+++ src/store/reducers/index.ts
@@ -3,5 +3,5 @@ import { fetchApiReducer } from './fetch-api'
 import { IStoreState } from '../../interfaces/store'
 
 export const reducers = combineReducers<IStoreState>({
-  fetchApi: fetchApiReducer,
+  fetchApiData: fetchApiReducer,
 })
