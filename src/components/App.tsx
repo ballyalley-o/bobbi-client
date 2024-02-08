23c916151f1b51ae8fc6333f8641ae23efb50892
bally <108769285+ballyalley-o@users.noreply.github.com>
2024-02-09 09:04:53 +1300

    Add API fetch functionality to App component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,26 @@
 import { Component } from 'react'
+import { connect } from 'react-redux'
+// @interfaces
+import { IApiFetch } from '@interfaces/api-fetch'
+import { IStoreState } from '@interfaces/store'
+import { IApp } from '@interfaces/component'
+// @reducers
+import { fetchApi } from '../store/actions'
 
-class App extends Component {
+class _App extends Component<IApp> {
+  componentDidMount(): void {
+    this.props.fetchApi()
+  }
+
   render() {
     return <h1>Bobbi Client</h1>
   }
 }
 
-export default App
+const mapStateToApp = ({
+  fetchApiData,
+}: IStoreState): { fetchApiData: IApiFetch[] } => {
+  return { fetchApiData }
+}
+
+export const App = connect(mapStateToApp, { fetchApi })(_App)
