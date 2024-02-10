97a0c9a9571a86cc306ece91c55032b5d3061cab
bally <108769285+ballyalley-o@users.noreply.github.com>
2024-02-11 04:48:09 +1300

    Refactor App component to include fetch limit input and loading state

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,17 +3,38 @@ import { connect } from 'react-redux'
 // @interfaces
 import { IApiFetch } from '@interfaces/api-fetch'
 import { IStoreState } from '@interfaces/store'
-import { IApp } from '@interfaces/component'
+import { IApp, IAppState, IAppFetchLimit } from '@interfaces/component'
 // @reducers
 import { fetchApi, deleteApi } from '../store/actions'
 
-class _App extends Component<IApp> {
+class _App extends Component<IApp, IAppState> {
+  fetchLimit: IAppFetchLimit | undefined
+
+  constructor(props: IApp) {
+    super(props)
+
+    this.state = { fetching: false }
+  }
+
   onButtonClick = (): void => {
-    this.props.fetchApi()
+    if (this.fetchLimit) this.props.fetchApi(this.fetchLimit)
+    this.setState({ fetching: true })
   }
 
   onDeleteClick = (id: number): void => {
     this.props.deleteApi(id)
+  }
+
+  handleInputChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    const newFetchLimit = parseInt(event.target.value, 10)
+
+    this.fetchLimit = { fetchLimit: newFetchLimit }
+  }
+
+  componentDidUpdate(prevProps: IApp): void {
+    if (!prevProps.fetchApiData.length && this.props.fetchApiData.length) {
+      this.setState({ fetching: false })
+    }
   }
 
   renderList(): JSX.Element[] {
@@ -30,12 +51,32 @@ class _App extends Component<IApp> {
   render() {
     return (
       <div>
-        <button onClick={this.onButtonClick}>Fetch Api</button>
+        <span>
+          <input
+            placeholder='enter limit'
+            type='number'
+            onChange={this.handleInputChange}
+            value={this.fetchLimit && this.fetchLimit.fetchLimit}
+          />
+        </span>
+        <button
+          // if the input value changes, update the fetchLimit or remove the disabled
+          disabled={this.props.fetchApiData.length > 0 && !this.fetchLimit}
+          onClick={this.onButtonClick}
+        >
+          Fetch Api
+        </button>
         <div
           style={{
             fontFamily: 'monospace',
           }}
         >
+          <h2>
+            {this.state.fetching &&
+              this.props.fetchApiData.length &&
+              !this.fetchLimit &&
+              'Loading...'}
+          </h2>
           <h3>{this.renderList()}</h3>
         </div>
       </div>
@@ -49,4 +90,7 @@ const mapStateToApp = ({
   return { fetchApiData }
 }
 
-export const App = connect(mapStateToApp, { fetchApi, deleteApi })(_App)
+export const App = connect(mapStateToApp, {
+  fetchApi,
+  deleteApi,
+})(_App)
