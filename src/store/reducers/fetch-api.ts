78c5dbe7e56509a313a48c1a193d9dab08f24f8f
bally <108769285+ballyalley-o@users.noreply.github.com>
2024-02-11 03:27:02 +1300

    Refactor fetchApiReducer to handle apiDelete action

--- src/store/reducers/fetch-api.ts
+++ src/store/reducers/fetch-api.ts
@@ -1,13 +1,13 @@
-import { IApiFetch, IApiFetchAction } from '@interfaces/api-fetch'
+import { IApiFetch } from '@interfaces/api-fetch'
 import { ActionType } from '../../constants/enum'
+import { Action } from '@typings'
 
-export const fetchApiReducer = (
-  state: IApiFetch[] = [],
-  action: IApiFetchAction
-) => {
+export const fetchApiReducer = (state: IApiFetch[] = [], action: Action) => {
   switch (action.type) {
     case ActionType.apifetch:
       return action.payload
+    case ActionType.apiDelete:
+      return state.filter((item: IApiFetch) => item.id !== action.payload)
     default:
       return state
   }
