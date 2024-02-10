e1e89b43a4ab0de5c06ac68a70c1318d01c3f314
bally <108769285+ballyalley-o@users.noreply.github.com>
2024-02-11 04:48:19 +1300

    Add fetchLimit parameter to fetchApi action

--- src/store/actions/index.ts
+++ src/store/actions/index.ts
@@ -2,15 +2,20 @@ import axios from 'axios'
 import { Dispatch } from 'redux'
 import { GLOBAL } from '../../config'
 import { ActionType } from '../../constants/enum'
+import { IApp, IAppFetchLimit } from '../../interfaces/component'
 import {
   IApiFetch,
   IApiFetchAction,
   IApiDeleteAction,
 } from '../../interfaces/api-fetch'
 
-export const fetchApi = () => {
+export const fetchApi = (limit: IAppFetchLimit) => {
   return async (dispatch: Dispatch) => {
-    const response = await axios.get<IApiFetch[]>(GLOBAL.API_MOCK(5) || '')
+    // wire up the fetchLimit input to the fetchApi action
+    const fetchLimit = limit.fetchLimit
+    const response = await axios.get<IApiFetch[]>(
+      GLOBAL.API_MOCK(fetchLimit) || ''
+    )
 
     dispatch<IApiFetchAction>({
       type: ActionType.apifetch,
